39dda5480c9886009f2120d92ed45ef10588e862
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-17 10:15:50 +0200

    feat: add external_reference_id to Order for 2026-04-16

--- resource/order.go
+++ resource/order.go
@@ -19,7 +19,8 @@ type Order struct {
 	Items             []orderitem.OrderItem `json:"items"`
 	AllowBackOrdering bool                  `json:"allow_back_ordering,omitempty"`
 	CallbackURL       *string               `json:"callback_url,omitempty"`
-	CallbackMethod    *string               `json:"callback_method,omitempty"`
+	CallbackMethod      *string               `json:"callback_method,omitempty"`
+	ExternalReferenceID *string               `json:"external_reference_id,omitempty"`
 }
 
 // UnmarshalJSON implements custom unmarshaling for Order.
