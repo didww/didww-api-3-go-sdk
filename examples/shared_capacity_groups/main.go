292e104386eb893f9fdcbda40a791baba31e7711
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-17 10:34:50 +0200

    examples: shared_capacity_groups - display external_reference_id

--- examples/shared_capacity_groups/main.go
+++ examples/shared_capacity_groups/main.go
@@ -27,11 +27,14 @@ func main() {
 	pool := pools[0]
 
 	// Create a shared capacity group
+	ts := time.Now().UnixMilli()
+	extRef := fmt.Sprintf("go-scg-%d", ts)
 	group := &resource.SharedCapacityGroup{
-		Name:                 fmt.Sprintf("SDK Channel Group %d", time.Now().UnixMilli()),
+		Name:                 fmt.Sprintf("SDK Channel Group %d", ts),
 		MeteredChannelsCount: 10,
 		SharedChannelsCount:  1,
 		CapacityPoolID:       pool.ID,
+		ExternalReferenceID:  &extRef,
 	}
 	created, err := client.SharedCapacityGroups().Create(ctx, group)
 	if err != nil {
@@ -39,4 +42,7 @@ func main() {
 	}
 	fmt.Printf("Created: %s name=%s metered=%d shared=%d\n",
 		created.ID, created.Name, created.MeteredChannelsCount, created.SharedChannelsCount)
+	if created.ExternalReferenceID != nil {
+		fmt.Printf("  External Reference: %s\n", *created.ExternalReferenceID)
+	}
 }
