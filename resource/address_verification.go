7a4e494e5b41f75cc7dc399684cda2ec9668d9bb
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-17 09:55:12 +0200

    feat!: address_verifications#reject_reasons is now an array of strings

--- resource/address_verification.go
+++ resource/address_verification.go
@@ -1,8 +1,6 @@
 package resource
 
 import (
-	"encoding/json"
-	"strings"
 	"time"
 
 	"github.com/didww/didww-api-3-go-sdk/resource/enums"
@@ -23,30 +21,4 @@ type AddressVerification struct {
 	DIDIDs    []string `json:"-" rel:"dids,dids"`
 	// Resolved relationships
 	AddressRel *Address `json:"-" rel:"address"`
-}
-
-// UnmarshalJSON splits the semicolon-separated reject_reasons string into a slice.
-func (a *AddressVerification) UnmarshalJSON(data []byte) error {
-	type Alias AddressVerification
-	aux := &struct {
-		RejectReasons *string `json:"reject_reasons"`
-		*Alias
-	}{
-		Alias: (*Alias)(a),
-	}
-	if err := json.Unmarshal(data, aux); err != nil {
-		return err
-	}
-	if aux.RejectReasons != nil {
-		rawItems := strings.Split(*aux.RejectReasons, "; ")
-		a.RejectReasons = make([]string, 0, len(rawItems))
-		for _, item := range rawItems {
-			if item != "" {
-				a.RejectReasons = append(a.RejectReasons, item)
-			}
-		}
-	} else {
-		a.RejectReasons = nil
-	}
-	return nil
 }
