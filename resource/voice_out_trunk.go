1c0c59c60b84019fbad5fd63cba0d2b2ced64299
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-17 10:21:06 +0200

    feat: add external_reference_id to VoiceOutTrunk for 2026-04-16

--- resource/voice_out_trunk.go
+++ resource/voice_out_trunk.go
@@ -26,6 +26,7 @@ type VoiceOutTrunk struct {
 	ForceSymmetricRtp    bool                                       `json:"force_symmetric_rtp,omitempty"`
 	RtpPing              bool                                       `json:"rtp_ping,omitempty"`
 	CallbackURL          *string                                    `json:"callback_url,omitempty"`
+	ExternalReferenceID  *string                                    `json:"external_reference_id,omitempty"`
 	AuthenticationMethod authenticationmethod.AuthenticationMethod   `json:"-"`
 	// Relationship IDs for create/update
 	DefaultDIDID string   `json:"-" rel:"default_did,dids"`
